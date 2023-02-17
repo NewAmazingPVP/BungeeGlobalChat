69e6874f8a8b57853607be9e1aa3e5a876b79985
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2023-02-17 08:37:09 -0500

    Initial commit

---
+++ src/main/java/bungeeglobalchat/bungeeglobalchat/BungeeGlobalChat.java
@@ -0,0 +1,37 @@
+package bungeeglobalchat.bungeeglobalchat;
+
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.event.ChatEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class BungeeGlobalChat extends Plugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getProxy().getPluginManager().registerListener(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+    public void onChat(ChatEvent event) {
+        if (event.isCommand()) {
+            return;
+        }
+
+        String message = event.getMessage();
+        String name = event.getSender().getName();
+        String serverName = event.getSender().getServer().getInfo().getName();
+
+        TextComponent formattedMessage = new TextComponent("[" + serverName + "] <" + name + "> " + message);
+        formattedMessage.setColor(ChatColor.GRAY);
+
+        getProxy().broadcast(formattedMessage);
+
+        // Cancel the chat event so that the message is not sent locally
+        event.setCancelled(true);
+    }
+}
